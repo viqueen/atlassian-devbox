a50102dc21addce2602e8ec0ff8043e1b5725df9
Hasnae <hasnae@viqueen.org>
2022-07-11 22:38:47 +1000

    welcome space available operations for logged in user

--- confluence-devbox/plugin/src/main/java/org/viqueen/atlassian/confluence/rest/OperationResource.java
+++ confluence-devbox/plugin/src/main/java/org/viqueen/atlassian/confluence/rest/OperationResource.java
@@ -4,6 +4,7 @@ import com.atlassian.confluence.api.model.content.id.ContentId;
 import com.atlassian.confluence.api.model.permissions.OperationCheckResult;
 import com.atlassian.confluence.api.model.permissions.Target;
 import com.atlassian.confluence.api.service.content.ContentService;
+import com.atlassian.confluence.api.service.content.SpaceService;
 import com.atlassian.confluence.api.service.permissions.OperationService;
 import com.atlassian.confluence.rest.api.model.ExpansionsParser;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
@@ -13,6 +14,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.Collections;
 import java.util.List;
@@ -21,24 +23,38 @@ import java.util.List;
 public class OperationResource {
 
     private final ContentService contentService;
+    private final SpaceService spaceService;
     private final OperationService operationService;
 
     @Autowired
     public OperationResource(
             @ComponentImport final ContentService contentService,
+            @ComponentImport final SpaceService spaceService,
             @ComponentImport final OperationService operationService
     ) {
         this.contentService = contentService;
+        this.spaceService = spaceService;
         this.operationService = operationService;
     }
 
     @GET
     @Path("/content/{id}")
-    @Produces("application/json")
-    public Response contentWithId(@PathParam("id") long id) {
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response contentWithId(@PathParam("id") final long id) {
         List<OperationCheckResult> operationCheckResults = contentService.find(ExpansionsParser.parse("container")).withId(ContentId.of(id))
                 .fetch()
                 .map(content -> operationService.getAvailableOperations(Target.forModelObject(content)))
+                .orElse(Collections.emptyList());
+        return Response.ok(operationCheckResults).build();
+    }
+
+    @GET
+    @Path("/space/{spaceKey}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response spaceWithKey(@PathParam("spaceKey") final String spaceKey) {
+        List<OperationCheckResult> operationCheckResults = spaceService.find().withKeys(spaceKey)
+                .fetch()
+                .map(space -> operationService.getAvailableOperations(Target.forModelObject(space)))
                 .orElse(Collections.emptyList());
         return Response.ok(operationCheckResults).build();
     }
