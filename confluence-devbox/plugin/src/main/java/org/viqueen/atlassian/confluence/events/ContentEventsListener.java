c3fdd738ef5186f7bf6f5d03b431f2a674f6c2e9
Hasnae <hasnae@viqueen.org>
2023-01-20 10:23:36 +1100

    example content restore events

---
+++ confluence-devbox/plugin/src/main/java/org/viqueen/atlassian/confluence/events/ContentEventsListener.java
@@ -0,0 +1,54 @@
+package org.viqueen.atlassian.confluence.events;
+
+import com.atlassian.confluence.event.events.content.ContentRevertedEvent;
+import com.atlassian.confluence.event.events.content.blogpost.BlogPostRestoreEvent;
+import com.atlassian.confluence.event.events.content.page.PageRestoreEvent;
+import com.atlassian.confluence.pages.Page;
+import com.atlassian.event.api.EventListener;
+import com.atlassian.event.api.EventListenerRegistrar;
+import com.atlassian.event.api.EventPublisher;
+import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import static java.lang.String.format;
+
+@Component
+public class ContentEventsListener implements InitializingBean, DisposableBean {
+
+    private final EventListenerRegistrar eventListenerRegistrar;
+
+    @Autowired
+    public ContentEventsListener(final @ComponentImport EventListenerRegistrar eventListenerRegistrar) {
+        this.eventListenerRegistrar = eventListenerRegistrar;
+    }
+
+    @EventListener
+    public void handlePageRestored(final PageRestoreEvent event) {
+        System.out.printf("**** restored from trash %s\n", event);
+    }
+
+    @EventListener
+    public void handleBlogRestored(final BlogPostRestoreEvent event) {
+        System.out.printf("**** restored blog from trash %s\n", event);
+    }
+
+    @EventListener
+    public void handleContentReverted(final ContentRevertedEvent event) {
+        System.out.printf("***** restored content version from history %s\n", event);
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        this.eventListenerRegistrar.register(this);
+    }
+
+    @Override
+    public void destroy() {
+        this.eventListenerRegistrar.unregister(this);
+    }
+}
